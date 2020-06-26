75961f4046d90a9687bae9b9c3585bda4cee6eba
Simone Securo <simone.securo@conse.it>
2020-06-26 14:50:30 +0000

    primo aggiornamento per poc

---
+++ webapp/model/OdataService.js
@@ -0,0 +1,11 @@
+sap.ui.define([], function () {
+	"use strict";
+
+	return {
+		getUrl: function () {
+			// return "http://java-app-quarkus.apps.poc-oc.poc.oc/moro_hub/odata/";
+			return "/MOROCOLOR_HUB/";
+			// return "https://morohubdevdng34zaaob.eu3.hana.ondemand.com/moro_hub/odata/";
+		}
+	};
+});
