2d4bb357171376862b594da49855749d7e52966c
Simone Securo <simone.securo@conse.it>
2020-07-02 08:02:52 +0000

    aggiunte condizioni per visualizzazione tile

--- webapp/model/OdataService.js
+++ webapp/model/OdataService.js
@@ -5,8 +5,13 @@ sap.ui.define([], function () {
 		getUrl: function () {
 			// return "/MOROCOLOR_HUB/";
 			// return "https://morohubdevdng34zaaob.eu3.hana.ondemand.com/moro_hub/odata/";
-			// return "http://java-app-quarkus.apps.poc-oc.poc.oc/moro_hub/odata/";	
-			return "http://quarkus-sap-mes-gpa-sap-poc.apps.poc-oc.poc.oc/rest/";		
+			// return "http://java-app-quarkus.apps.poc-oc.poc.oc/moro_hub/odata/";
+			var hostname = location.hostname;
+			if (hostname.indexOf("webidetesting") > 0) {
+				return "/MOROCOLOR_HUB/";
+			} else {
+				return "http://quarkus-sap-mes-gpa-sap-poc.apps.poc-oc.poc.oc/rest/";
+			}
 		}
 	};
 });
